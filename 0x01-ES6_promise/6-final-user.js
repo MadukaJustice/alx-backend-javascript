e31150e73278e12f2469ceb9ca41d5899fc60de2
MadukaJustice <jonasjustice98@gmail.com>
2023-06-28 09:25:16 +0000

    Understanding promise prototype

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,6 +1,11 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+// return multiple promises
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return (Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]));
-}
+const handleProfileSignup = (firstName, lastName, fileName) => {
+    return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then(value => value.map(result => ( { status: result.status, 
+                                            value: result.value })))
+};
+
+export default handleProfileSignup;

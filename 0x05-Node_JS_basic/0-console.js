099f1b1377af0a5b2d395c008a7a554c4cfba3cb
MadukaJustice <jonasjustice98@gmail.com>
2023-08-21 20:20:35 +0000

    prints in STDOUT

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -2,8 +2,9 @@
  * A function that prints in STDOUT the message passed as parameter
  * @param message - the message to print
  */
-function displayMessage(message) {
-  console.log(message);
+
+function displayMessage(d) {
+  process.stdout.write(`${d}\n`);
 }
 
 module.exports = displayMessage;

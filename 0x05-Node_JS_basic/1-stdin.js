dc6bbc27de10cdbf000881a8156354836a81a765
MadukaJustice <jonasjustice98@gmail.com>
2023-08-22 14:10:07 +0000

    Reading a csv file

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -9,12 +9,18 @@
  * Your name is: <name>
  */
 
-console.log('Welcome to Holberton School, what is your name?');
+const displayMessage = require('./0-console');
+
+const message = 'Welcome to Holberton School, what is your name?';
+
+displayMessage(message);
+
+process.stdin.setEncoding('utf-8');
 
 process.stdin.on('readable', () => {
-  const name = process.stdin.read();
-  if (name !== null) {
-    process.stdout.write(`Your name is: ${name}`);
+  const input = process.stdin.read();
+  if (input !== null) {
+    process.stdout.write(`Your name is: ${input}`);
   }
 });
 
